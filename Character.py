575b1e562882a42ddc220918253e224478a9caeb
Diamond-Bagel <72178852+Diamond-Bagel@users.noreply.github.com>
2020-10-09 01:19:56 -0700

    Delete Character.py

--- Character.py
+++
@@ -1,8 +0,0 @@
-import movement, Shop
-character = {
-    'hit-points': 100,
-    'location': movement.locations[4],
-    'inventory': ['Wooden Sword', 'Wooden Shield', 'Magical Amulet'],
-    'photo': "▼",
-}
-coins = 5
